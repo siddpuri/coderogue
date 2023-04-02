1511dd6b31e592006b7e096dd42065d7fb650bc2
Sidd Puri <sidd@studiopuri.com>
2023-04-02 04:33:59 +0000

    Thread players through database

--- db/reset.js
+++ db/reset.js
@@ -1,4 +1,26 @@
 import DB from './db.js';
 
+// For initial setup, run this script as root:
+//   CREATE DATABASE game;
+//   CREATE USER 'game'@'localhost' IDENTIFIED WITH mysql_native_password BY 'game';
+//   GRANT ALL PRIVILEGES ON game.* TO 'game'@'localhost';
+//   GRANT CREATE, DROP on *.* to 'game'@'localhost';
+//   FLUSH PRIVILEGES;
+
+const resetScript = [
+    "DROP DATABASE IF EXISTS game",
+    "CREATE DATABASE game",
+    "USE game",
+    `CREATE TABLE players (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        period INT NOT NULL,
+        handle INT NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        UNIQUE (handle)
+    )`,
+]
+
 const db = new DB();
-db.reset();
+
+for (let q of resetScript) await db.query(q);
