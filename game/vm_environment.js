6d3853218e1d1c1afe85bd76e8a5ac2e21404883
Sidd Puri <sidd@studiopuri.com>
2023-05-12 06:25:37 -0700

    Update sample.js

--- game/vm_environment.js
+++ game/vm_environment.js
@@ -3,37 +3,38 @@ export default class VmEnvironment {
         this.game = game;
         this.player = player;
         this.sandbox = {
-            // Overridden ES6 functionality
+            // General functionality
             console: {
                 log: text => this.log(text),
                 debug: text => console.debug(text),
             },
 
-            // General AppLab functions
-            randomNumber: (a, b) => Math.floor(Math.random() * (b - a + 1)) + a,
-            appendItem: (l, x) => l.push(x),
-            insertItem: (l, i, x) => l.splice(i, 0, x),
-            removeItem: (l, i) => l.splice(i, 1),
+            state: 'initial',
 
             // Robot movement
             moveForward: () => this.moveForward(),
             turnRight: () => this.turnRight(),
             turnLeft: () => this.turnLeft(),
             canMove: dir => this.canMove(dir),
+            respawn: () => this.game.killPlayer(this.player),
+            respawnAt: (level, pos, dir) => this.game.respawnAt(this.player, level, pos, dir),
 
             forward: 0,
             right: 1,
             backward: 2,
             left: 3,
 
-            respawn: () => this.game.killPlayer(this.player),
-            respawnAt: (level, pos, dir) => this.game.respawnAt(this.player, level, pos, dir),
-
             // Robot sensors
             getLevel: () => this.player.level.levelNumber,
             getPosition: () => this.player.pos,
             getDirection: () => this.player.dir,
             getExitPosition: () => this.player.level.exitPos,
+
+            // AppLab functions
+            randomNumber: (a, b) => Math.floor(Math.random() * (b - a + 1)) + a,
+            appendItem: (l, x) => l.push(x),
+            insertItem: (l, i, x) => l.splice(i, 0, x),
+            removeItem: (l, i) => l.splice(i, 1),
         };
     }
 
--- sample.js
+++ sample.js
@@ -2,22 +2,40 @@ var dx = getExitPosition()[0] - getPosition()[0];
 var dy = getExitPosition()[1] - getPosition()[1];
 var cd = getDirection();
 
-function goodDirection(dir) {
-    let realDir = (cd + dir) % 4;
-    let warmer =
-        realDir == 0 && dy < 0 ||
-        realDir == 1 && dx > 0 ||
-        realDir == 2 && dy > 0 ||
-        realDir == 3 && dx < 0;
-    return warmer && canMove(dir);
+if (state == 'initial') moveTowardExit();
+if (state == 'circumnavigate') {
+    circumnavigate();
+    if (state == 'initial') moveTowardExit();
 }
 
-if      (goodDirection(forward)) moveForward();
-else if (goodDirection(right)) turnRight();
-else if (goodDirection(left)) turnLeft();
-else if (goodDirection(backward)) turnRight();
-else if (canMove(forward)) moveForward();
-else if (canMove(right)) turnRight();
-else if (canMove(left)) turnLeft();
-else if (canMove(backward)) turnRight();
-else respawn();
+function moveTowardExit() {
+    if      (isGood(forward))  moveForward();
+    else if (isGood(right))    turnRight();
+    else if (isGood(left))     turnLeft();
+    else if (isGood(backward)) turnRight();
+    else if (!isWallOnLeft())  turnRight();
+    else                       state = 'circumnavigate';
+}
+
+function circumnavigate() {
+    if      (isGood(forward))  state = 'initial';
+    else if (isGood(left))     state = 'initial';
+    else if (canMove(left))    turnLeft();
+    else if (canMove(forward)) moveForward();
+}
+
+function isGood(dir) {
+    return isWarmer(dir) && canMove(dir);
+}
+
+function isWallOnLeft() {
+    return isWarmer(left) && !canMove(left);
+}
+
+function isWarmer(dir) {
+    let realDir = (cd + dir) % 4;
+    return realDir == 0 && dy < 0 ||
+           realDir == 1 && dx > 0 ||
+           realDir == 2 && dy > 0 ||
+           realDir == 3 && dx < 0;
+}
