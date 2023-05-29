305f579a7e16359026045282a5e28dbd0352fd24
Sidd Puri <sidd@studiopuri.com>
2023-05-29 16:51:41 -0700

    Handle async errors in web server

--- server/web_server.js
+++ server/web_server.js
@@ -18,44 +18,56 @@ export default class WebServer {
     this.app.use(express.static('client'));
     this.app.use(express.static('client/static'));
 
-    this.app.post('/api/login', async (req, res) => {
-      let response = await this.server.auth.login(req.body);
-      res.send(JSON.stringify(response));
+    this.app.post('/api/login', async (req, res, next) => {
+        try {
+            let response = await this.server.auth.login(req.body);
+            res.send(JSON.stringify(response));
+        } catch (err) {
+            next(err);
+        }
     });
 
     this.app.get('/api/state', (req, res) => {
-      let response = this.server.game.getState();
-      res.send(JSON.stringify(response));
+        let response = this.server.game.getState();
+        res.send(JSON.stringify(response));
     });
 
     this.app.post('/api/respawn', (req, res) => {
-      let playerId = this.validatePlayerId(req, res);
-      if (!playerId) return;
-      let player = this.server.game.players[playerId];
-      this.server.game.respawn(player);
-      res.send(JSON.stringify({}));
+        let playerId = this.validatePlayerId(req, res);
+        if (!playerId) return;
+        let player = this.server.game.players[playerId];
+        this.server.game.respawn(player);
+        res.send(JSON.stringify({}));
     });
 
     this.app.get('/api/code', async (req, res) => {
-      let playerId = this.validatePlayerId(req, res);
-      if (!playerId) return;
-      let code = await this.server.repositories.readCode(playerId);
-      res.send(JSON.stringify({ code }));
+        try {
+            let playerId = this.validatePlayerId(req, res);
+            if (!playerId) return;
+            let code = await this.server.repositories.readCode(playerId);
+            res.send(JSON.stringify({ code }));
+        } catch (err) {
+            next(err);
+        }
     });
 
     this.app.post('/api/code', async (req, res) => {
-      let playerId = this.validatePlayerId(req, res);
-      if (!playerId) return;
-      await this.server.repositories.writeCode(playerId, req.body.code);
-      this.server.game.players[playerId].onNewCode();
-      res.send(JSON.stringify({}));
+        try {
+            let playerId = this.validatePlayerId(req, res);
+            if (!playerId) return;
+            await this.server.repositories.writeCode(playerId, req.body.code);
+            this.server.game.players[playerId].onNewCode();
+            res.send(JSON.stringify({}));
+        } catch (err) {
+            next(err);
+        }
     });
 
     this.app.get('/api/log', (req, res) => {
-      let playerId = this.validatePlayerId(req, res);
-      if (!playerId) return;
-      let log = this.server.game.players[playerId].log.toString();
-      res.send(JSON.stringify({ log }));
+        let playerId = this.validatePlayerId(req, res);
+        if (!playerId) return;
+        let log = this.server.game.players[playerId].log.toString();
+        res.send(JSON.stringify({ log }));
     });
 
     this.app.post('/api/player', (req, res) => {
@@ -70,7 +82,7 @@ export default class WebServer {
     });
 
     this.app.listen(port, () => {
-      console.log(`Web server listening on port ${port}`);
+        console.log(`Web server listening on port ${port}`);
     });
   }
 
@@ -78,13 +90,13 @@ export default class WebServer {
     let playerId = req.cookies.playerId;
     let authToken = req.cookies.authToken;
     if (
-      !playerId ||
-      !authToken ||
-      !this.server.game.players[playerId] ||
-      this.server.game.players[playerId].authToken != authToken
+        !playerId ||
+        !authToken ||
+        !this.server.game.players[playerId] ||
+        this.server.game.players[playerId].authToken != authToken
     ) {
-      res.send(JSON.stringify({ error: 'Not logged in.' }));
-      return undefined;
+        res.send(JSON.stringify({ error: 'Not logged in.' }));
+        return undefined;
     }
     return playerId;
   }
