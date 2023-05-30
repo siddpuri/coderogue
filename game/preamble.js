7d2d7dc3e0a65e46b401ac13e7f5adb64438eedb
Sidd Puri <sidd@studiopuri.com>
2023-05-30 21:15:45 +0000

    Fix bug in whatsAt after respawn

--- game/preamble.js
+++ game/preamble.js
@@ -1,9 +1,13 @@
 // Uncomment the next line to see syntax highlighting.
 export default class Preamble { static get code() { return `
 
-var _map = getMap();
+var _map;
 
 function whatsAt(pos) {
+    if (!_map || getLevel() != _map.level) {
+        _map = getMap();
+        _map.level = getLevel();
+    }
     let [x, y] = pos;
     let code = _map[y * 80 + x];
     return String.fromCharCode(code?? 35);
