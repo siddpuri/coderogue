b87e20d94eef327d54fe99a19e3f08165c6f08c6
Sidd Puri <sidd@studiopuri.com>
2023-06-13 21:42:55 -0700

    Update grownups list

--- shared/grownups.js
+++ shared/grownups.js
@@ -1,11 +1,12 @@
 export default class Grownups {
     static get list() {
         return [
-            1,   // Sidd    shy-yellow-turtle
-            100, // Michael zany-pink-bird
-            155, // Joshua? cute-black-hamster
-            160, // Philip  silly-pink-rabbit
-            196, // KHallman shy-black-dog
+            1,   // Sidd        shy-yellow-turtle
+            100, // Michael     zany-pink-bird
+            155, // Joshua      cute-black-hamster
+            160, // Philip      silly-pink-rabbit
+            196, // KHallman    shy-black-dog
+            201, // Ethan's dad zany-black-fish
         ];
     }
 }
