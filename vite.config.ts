946437840ceb15a2ff99de6a17a5732ce0ecac51
Sidd Puri <sidd@studiopuri.com>
2023-07-24 11:27:02 -0700

    Get vite dev connected to backend server

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 8081,
+        proxy: { '/api': 'http://localhost:8080' },
+    }
 });
