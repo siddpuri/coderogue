cbfddfbaa0f4cbbc163b8c53a763d055ea8e6726
Sidd Puri <sidd@studiopuri.com>
2023-11-15 14:43:00 -0800

    Format according to Redux boilerplate

--- src/client/redux_hooks.ts
+++ src/client/redux_hooks.ts
@@ -1,6 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './redux_store';
 
-type DispatchFunc = () => AppDispatch;
-export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
