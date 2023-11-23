c36d425dbee07314d51be24c063218449f4f4be1
Sidd Puri <sidd@studiopuri.com>
2023-11-22 17:35:35 -0800

    Move tabs into their own components

--- src/client/components/tab_pane.tsx
+++ src/client/components/tab_pane.tsx
@@ -2,6 +2,8 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 import CodeTab from '../tabs/code_tab';
+import LogTab from '../tabs/log_tab';
+import PlayerTab from '../tabs/player_tab';
 import NewsTab from '../tabs/news_tab';
 import GeneralTab from '../tabs/general_tab';
 import ApiTab from '../tabs/api_tab';
@@ -9,106 +11,26 @@ import LevelsTab from '../tabs/levels_tab';
 import KeybindingsTab from '../tabs/keybindings_tab';
 import AccountTab from '../tabs/account_tab';
 
+const tabs = [
+    { name: 'Code', component: CodeTab },
+    { name: 'Log', component: LogTab },
+    { name: 'Player', component: PlayerTab },
+    { name: 'News', component: NewsTab },
+    { name: 'General', component: GeneralTab },
+    { name: 'Api', component: ApiTab },
+    { name: 'Levels', component: LevelsTab },
+    { name: 'Shortcuts', component: KeybindingsTab },
+    { name: 'Account', component: AccountTab },
+]
+
 export default function TabPane() {
     return (
-        <Tabs
-            defaultActiveKey="code"
-            id="tab-pane"
-            className="mt-3 mb-3"
-            justify>
-
-            <Tab eventKey="code" title="Code">
-                <CodeTab />
-            </Tab>
-
-            <Tab eventKey="log" title="Log">
-                <div className="row">
-                    <div className="col">
-                        <textarea className="form-control font-monospace" id="log-text" rows={20} />
-                    </div>
-                    <div className="col-2">
-                        <div className="d-grid gap-2">
-                            <button type="button" className="btn btn-secondary" id="respawn2">Respawn</button>
-                            <button type="button" className="btn btn-secondary" id="freeze">Freeze</button>
-                            <div className="btn-group-vertical">
-                                <button type="button" className="btn btn-secondary active" id="show-all">Show all</button>
-                                <button type="button" className="btn btn-secondary" id="show-latest">Just latest</button>
-                                <button type="button" className="btn btn-secondary" id="show-filtered">Filter by</button>
-                                <input type="text" className="form-control" id="filter-text" placeholder="Filter" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Tab>
-
-            <Tab eventKey="player" title="Player">
-                <div className="row d-flex justify-content-between">
-                    <div className="col-4">
-                        <table className="table" id="player-stats">
-                            <thead>
-                                <tr>
-                                    <th>Level</th>
-                                    <th className="table-col">J</th>
-                                    <th className="table-col">0</th>
-                                    <th className="table-col">1</th>
-                                    <th className="table-col">2</th>
-                                    <th className="table-col">3</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr><td>Seconds spent</td></tr>
-                                <tr><td>Times completed</td></tr>
-                                <tr><td>Score/second</td></tr>
-                                <tr><td>Time/completion</td></tr>
-                                <tr><td>Cumulative t/c</td></tr>
-                                <tr><td>Project goal met?</td></tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="col-4">
-                        <canvas id="player-chart" />
-                    </div>
-                    <div className="col-2">
-                        <table className="table" id="player-info">
-                            <tbody>
-                                <tr><td>Level</td><td className="table-col" /></tr>
-                                <tr><td>Position</td><td className="table-col" /></tr>
-                                <tr><td>Direction</td><td className="table-col" /></tr>
-                                <tr><td>Idle time</td><td className="table-col" /></tr>
-                                <tr><td>Offenses</td><td className="table-col" /></tr>
-                                <tr><td>Jail time</td><td className="table-col" /></tr>
-                                <tr><td>Player id</td><td className="table-col" /></tr>
-                                <tr><td>Handle #</td><td className="table-col" /></tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </Tab>
-
-            <Tab eventKey="news" title="News">
-                <NewsTab />
-            </Tab>
-
-            <Tab eventKey="general" title="General">
-                <GeneralTab />
-            </Tab>
-
-            <Tab eventKey="api" title="Api">
-                <ApiTab />
-            </Tab>
-
-            <Tab eventKey="levels" title="Levels">
-                <LevelsTab />
-            </Tab>
-
-            <Tab eventKey="keybindings" title="Shortcuts">
-                <KeybindingsTab />
-            </Tab>
-
-            <Tab eventKey="account" title="Account">
-                <AccountTab />
-            </Tab>
-
+        <Tabs defaultActiveKey="code" id="tab-pane" className="mt-3 mb-3" justify>
+            {tabs.map(({ name, component: Component }) => (
+                <Tab eventKey={name} title={name}>
+                    <Component />
+                </Tab>
+            ))}
         </Tabs>
     );
 }
---
+++ src/client/tabs/player_tab.tsx
@@ -0,0 +1,45 @@
+export default function PlayerTab() {
+    return (
+        <div className="row d-flex justify-content-between">
+            <div className="col-4">
+                <table className="table" id="player-stats">
+                    <thead>
+                        <tr>
+                            <th>Level</th>
+                            <th className="table-col">J</th>
+                            <th className="table-col">0</th>
+                            <th className="table-col">1</th>
+                            <th className="table-col">2</th>
+                            <th className="table-col">3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Seconds spent</td></tr>
+                        <tr><td>Times completed</td></tr>
+                        <tr><td>Score/second</td></tr>
+                        <tr><td>Time/completion</td></tr>
+                        <tr><td>Cumulative t/c</td></tr>
+                        <tr><td>Project goal met?</td></tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="col-4">
+                <canvas id="player-chart" />
+            </div>
+            <div className="col-2">
+                <table className="table" id="player-info">
+                    <tbody>
+                        <tr><td>Level</td><td className="table-col" /></tr>
+                        <tr><td>Position</td><td className="table-col" /></tr>
+                        <tr><td>Direction</td><td className="table-col" /></tr>
+                        <tr><td>Idle time</td><td className="table-col" /></tr>
+                        <tr><td>Offenses</td><td className="table-col" /></tr>
+                        <tr><td>Jail time</td><td className="table-col" /></tr>
+                        <tr><td>Player id</td><td className="table-col" /></tr>
+                        <tr><td>Handle #</td><td className="table-col" /></tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
