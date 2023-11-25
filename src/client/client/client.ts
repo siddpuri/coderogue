839ec76f2a13d20623171fa2edf03d6eae7a4484
Sidd Puri <sidd@studiopuri.com>
2023-11-24 23:46:42 -0800

    Remove fetcher and button hooks

--- src/client/client/client.ts
+++ src/client/client/client.ts
@@ -1,4 +1,3 @@
-import ButtonHooks from './button_hooks.js';
 import Credentials from './credentials.js';
 import Display from './display.js';
 
@@ -6,17 +5,14 @@ export default class Client {
     readonly baseUrl = window.location.origin;
     readonly display: Display;
     readonly credentials: Credentials;
-    readonly buttonHooks: ButtonHooks;
 
     constructor() {
         this.display = new Display(this);
         this.credentials = new Credentials(this);
-        this.buttonHooks = new ButtonHooks(this);
     }
 
     async start(): Promise<void> {
         await this.display.start();
         await this.credentials.start();
-        await this.buttonHooks.start();
     }
 }
