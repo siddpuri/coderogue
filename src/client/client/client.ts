adc49aadff77615086d4dee77fd0489f476be2be
Sidd Puri <sidd@studiopuri.com>
2023-11-25 00:07:43 -0800

    Remove deprecated Credentials store

--- src/client/client/client.ts
+++ src/client/client/client.ts
@@ -1,18 +1,14 @@
-import Credentials from './credentials.js';
 import Display from './display.js';
 
 export default class Client {
     readonly baseUrl = window.location.origin;
     readonly display: Display;
-    readonly credentials: Credentials;
 
     constructor() {
         this.display = new Display(this);
-        this.credentials = new Credentials(this);
     }
 
     async start(): Promise<void> {
         await this.display.start();
-        await this.credentials.start();
     }
 }
