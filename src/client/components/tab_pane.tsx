c92e9fcc8e0f93809bb3a777ab58e12680374618
Sidd Puri <sidd@studiopuri.com>
2023-11-29 11:15:50 -0800

    Reorder tabs

--- src/client/components/tab_pane.tsx
+++ src/client/components/tab_pane.tsx
@@ -1,34 +1,48 @@
+import { useState, useEffect } from 'react';
+import { useAppSelector } from '../client/redux_hooks';
+
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 import Markdown from 'react-markdown'
 
+import generalMarkdown from '../assets/general_tab.md?raw';
+import ApiTab from '../tabs/api_tab';
+import KeybindingsTab from '../tabs/keybindings_tab';
+import levelsMarkdown from '../assets/levels_tab.md?raw';
+import newsMarkdown from '../assets/news_tab.md?raw';
+
 import CodeTab from '../tabs/code_tab';
 import LogTab from '../tabs/log_tab';
 import PlayerTab from '../tabs/player_tab';
-import ApiTab from '../tabs/api_tab';
-import KeybindingsTab from '../tabs/keybindings_tab';
 import AccountTab from '../tabs/account_tab';
 
-import generalMarkdown from '../assets/general_tab.md?raw';
-import newsMarkdown from '../assets/news_tab.md?raw';
-import levelsMarkdown from '../assets/levels_tab.md?raw';
-
 const tabs = [
+    { name: 'General', component: <Markdown>{generalMarkdown}</Markdown> },
+    { name: 'Api', component: <ApiTab /> },
+    { name: 'Shortcuts', component: <KeybindingsTab /> },
+    { name: 'Levels', component: <Markdown>{levelsMarkdown}</Markdown> },
+    { name: 'News', component: <Markdown>{newsMarkdown}</Markdown> },
     { name: 'Code', component: <CodeTab /> },
     { name: 'Log', component: <LogTab /> },
     { name: 'Player', component: <PlayerTab /> },
-    { name: 'News', component: <Markdown>{newsMarkdown}</Markdown> },
-    { name: 'General', component: <Markdown>{generalMarkdown}</Markdown> },
-    { name: 'Api', component: <ApiTab /> },
-    { name: 'Levels', component: <Markdown>{levelsMarkdown}</Markdown> },
-    { name: 'Shortcuts', component: <KeybindingsTab /> },
     { name: 'Account', component: <AccountTab /> },
 ];
 
 export default function TabPane() {
+    const currentPlayer = useAppSelector(state => state.login?.credentials?.playerId ?? null);
+
+    const [key, setKey] = useState<string | null>('General');
+
+    useEffect(() => { if (currentPlayer) setKey('Code'); }, []);
+
     return <>
-        <Tabs defaultActiveKey="Code" id="tab-pane" className="mt-3 mb-3" justify>
+        <Tabs
+            activeKey={key ?? undefined}
+            onSelect={k => setKey(k)}
+            className="mt-3 mb-3"
+            justify
+        >
             {tabs.map(({ name, component }) => (
                 <Tab key={name} eventKey={name} title={name}>
                     {component}
