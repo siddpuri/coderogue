8fefa7db38dbded8dfdbbe84cf6fc67a407d401f
Sidd Puri <sidd@studiopuri.com>
2023-12-07 02:11:07 -0800

    Get Player tab working again

--- src/client/components/player_tab.tsx
+++ src/client/components/player_tab.tsx
@@ -1,4 +1,58 @@
+import { useAppSelector } from '../client/redux_hooks';
+
+import { useGetStateQuery } from '../state/server_api';
+
+import { PlayerData } from '../../shared/protocol';
+import Util from '../../shared/util';
+
+type RowInfo = {
+    label: string,
+    values: number[],
+    precision?: number,
+};
+
+type PlayerInfo = {
+    label: string,
+    value: number | [number, number],
+};
+
 export default function PlayerTab() {
+    const gameState = useGetStateQuery()?.data;
+    const currentPlayer = useAppSelector(state => state.login?.credentials?.playerId ?? null);
+    const display = useAppSelector(state => state.display);
+
+    let player = display.highlightedPlayer ?? currentPlayer;
+    if (player == null || !gameState?.players[player]) return null;
+    let stats = gameState?.players[player] as PlayerData;
+    if (!stats) return null;
+
+    let scorePerSecond = stats.score.map((x, i) => x / stats.timeSpent[i]);
+    let timePerCompletion = stats.timesCompleted.map((x, i) => x? stats.timeSpent[i] / x : 0);
+    let cumulativeTimePerCompletion = timePerCompletion.slice();
+    for (let i = 1; i < cumulativeTimePerCompletion.length; i++) {
+        cumulativeTimePerCompletion[i] += cumulativeTimePerCompletion[i - 1];
+    }
+
+    let columns = ['J', '0', '1', '2', '3'];
+    let rows: RowInfo[] = [
+        { label: 'Seconds spent', values: stats.timeSpent },
+        { label: 'Times completed', values: stats.timesCompleted },
+        { label: 'Score/second', values: scorePerSecond, precision: 2 },
+        { label: 'Time/completion', values: timePerCompletion },
+        { label: 'Cumulative t/c', values: cumulativeTimePerCompletion },
+    ];
+
+    let info: PlayerInfo[] = [
+        { label: 'Level', value: stats.levelNumber },
+        { label: 'Position', value: stats.pos },
+        { label: 'Direction', value: stats.dir },
+        { label: 'Idle time', value: stats.idle },
+        { label: 'Offenses', value: stats.offenses },
+        { label: 'Jail time', value: stats.jailtime },
+        { label: 'Player id', value: stats.id },
+        { label: 'Handle #', value: stats.handle },
+    ]
+    
     return <>
         <div className="row d-flex justify-content-between">
             <div className="col-4">
@@ -6,20 +60,11 @@ export default function PlayerTab() {
                     <thead>
                         <tr>
                             <th>Level</th>
-                            <th className="table-col">J</th>
-                            <th className="table-col">0</th>
-                            <th className="table-col">1</th>
-                            <th className="table-col">2</th>
-                            <th className="table-col">3</th>
+                            {columns.map((s, i) => <th className="table-col" key={i}>{s}</th>)}
                         </tr>
                     </thead>
                     <tbody>
-                        <tr><td>Seconds spent</td></tr>
-                        <tr><td>Times completed</td></tr>
-                        <tr><td>Score/second</td></tr>
-                        <tr><td>Time/completion</td></tr>
-                        <tr><td>Cumulative t/c</td></tr>
-                        <tr><td>Project goal met?</td></tr>
+                        {rows.map(renderRow)}
                     </tbody>
                 </table>
             </div>
@@ -29,17 +74,31 @@ export default function PlayerTab() {
             <div className="col-2">
                 <table className="table" id="player-info">
                     <tbody>
-                        <tr><td>Level</td><td className="table-col" /></tr>
-                        <tr><td>Position</td><td className="table-col" /></tr>
-                        <tr><td>Direction</td><td className="table-col" /></tr>
-                        <tr><td>Idle time</td><td className="table-col" /></tr>
-                        <tr><td>Offenses</td><td className="table-col" /></tr>
-                        <tr><td>Jail time</td><td className="table-col" /></tr>
-                        <tr><td>Player id</td><td className="table-col" /></tr>
-                        <tr><td>Handle #</td><td className="table-col" /></tr>
+                        {info.map(renderInfo)}
                     </tbody>
                 </table>
             </div>
         </div>
     </>;
+
+    function renderRow(row: RowInfo): JSX.Element | null {
+        return <>
+            <tr key={row.label}>
+                <td style={{whiteSpace: "nowrap"}}>{row.label}</td>
+                {(row.values ?? []).map((x, i) => {
+                    if (!x) return <td key={i} />;
+                    return <td key={i} className="table-col">{x.toFixed(row.precision)}</td>
+                })}
+            </tr>
+        </>;
+    }
+
+    function renderInfo(info: PlayerInfo): JSX.Element {
+        return <>
+            <tr key={info.label}>
+                <td>{info.label}</td>
+                <td className="table-col">{Util.stringify(info.value)}</td>
+            </tr>
+        </>;
+    }
 }
